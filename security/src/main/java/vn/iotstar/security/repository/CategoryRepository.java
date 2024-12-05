93f2effb13bef2ef55bb3e4bd1d9ee0aaf52f9fe
Tiendvt <dovantientk@gmail.com>
2024-12-05 09:47:45 +0700

    home controller

---
+++ security/src/main/java/vn/iotstar/security/repository/CategoryRepository.java
@@ -0,0 +1,18 @@
+package vn.iotstar.security.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import vn.iotstar.security.model.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	public Boolean existsByName(String name);
+
+	public List<Category> findByIsActiveTrue();
+
+	public Category findByName(String name);
+
+}
