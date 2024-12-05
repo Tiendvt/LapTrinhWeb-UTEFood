a1fb4bc838aa782347e7c668478141af3a53cfac
Krant274 <thctan2704@proton.me>
2024-12-05 17:09:43 +0700

    add 'edit/delete category, products', 'create admin', 'profile'

--- security/src/main/java/vn/iotstar/security/service/impl/UserServiceImpl.java
+++ security/src/main/java/vn/iotstar/security/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package vn.iotstar.security.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import vn.iotstar.security.model.User;
 import vn.iotstar.security.repository.UserRepository;
@@ -44,6 +47,46 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public User updateUser(User user) {
 		return userRepository.save(user);
+	}
+
+	@Override
+	public List<User> getUsers(String role) {
+		return userRepository.findByRole(role);
+	}
+
+	@Override
+	public Boolean updateAccountStatus(Integer id, Boolean status) {
+		Optional<User> findByuser = userRepository.findById(id);
+
+		if (findByuser.isPresent()) {
+			User user = findByuser.get();
+			user.setIsEnable(status);
+			userRepository.save(user);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public User saveAdmin(User user) {
+		user.setRole("ROLE_ADMIN");
+		user.setIsEnable(true);
+		user.setAccountNonLocked(true);
+		user.setFailedAttempt(0);
+
+		String encodePassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodePassword);
+		User saveUser = userRepository.save(user);
+		return saveUser;
+	}
+
+	@Override
+	public User updateUserProfile(User user, MultipartFile img) {
+		User dbUser = userRepository.findById(user.getId()).get();
+
+		
+
+		return dbUser;
 	}
 	
 }
