406ec912cfb5731283b61847476743640cdf9ba9
tri2703 <triute2703@gmail.com>
2024-12-05 09:59:44 +0700

    giao dien trang dang nhap va dang ky
    update lại giao diện

--- security/src/main/java/vn/iotstar/security/controllers/AdminController.java
+++ security/src/main/java/vn/iotstar/security/controllers/AdminController.java
@@ -1,14 +1,163 @@
 package vn.iotstar.security.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import jakarta.servlet.http.HttpSession;
+import vn.iotstar.security.model.Category;
+import vn.iotstar.security.model.Product;
+import vn.iotstar.security.service.CategoryService;
+import vn.iotstar.security.service.ProductService;
+
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@Autowired
+	private ProductService productService;
+	
     @GetMapping("/")
     public String index() {
         return "admin/index"; 
     }
+    
+    @GetMapping("/loadAddProduct")
+	public String loadAddProduct(Model m) {
+		List<Category> categories = categoryService.getAllCategory();
+		m.addAttribute("categories", categories);
+		return "admin/add_product";
+	}
+    
+    @GetMapping("/category")
+	public String category(Model m, @RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+		// m.addAttribute("categorys", categoryService.getAllCategory());
+		Page<Category> page = categoryService.getAllCategorPagination(pageNo, pageSize);
+		List<Category> categorys = page.getContent();
+		m.addAttribute("categorys", categorys);
+
+		m.addAttribute("pageNo", page.getNumber());
+		m.addAttribute("pageSize", pageSize);
+		m.addAttribute("totalElements", page.getTotalElements());
+		m.addAttribute("totalPages", page.getTotalPages());
+		m.addAttribute("isFirst", page.isFirst());
+		m.addAttribute("isLast", page.isLast());
+
+		return "admin/category";
+	}
+    
+    @PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+			HttpSession session) throws IOException {
+
+		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
+		category.setImageName(imageName);
+
+		Boolean existCategory = categoryService.existCategory(category.getName());
+
+		if (existCategory) {
+			session.setAttribute("errorMsg", "Category Name already exists");
+		} else {
+
+			Category saveCategory = categoryService.saveCategory(category);
+
+			if (ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Not saved ! internal server error");
+			} else {
+
+				File saveFile = new ClassPathResource("static/product_img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+						+ file.getOriginalFilename());
+
+				// System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				session.setAttribute("succMsg", "Saved successfully");
+			}
+		}
+
+		return "redirect:/admin/category";
+	}
+    
+    @PostMapping("/saveProduct")
+	public String saveProduct(@ModelAttribute Product product, @RequestParam("file") MultipartFile image,
+			HttpSession session) throws IOException {
+
+		String imageName = image.isEmpty() ? "default.jpg" : image.getOriginalFilename();
+
+		product.setImage(imageName);
+		product.setDiscount(0);
+		product.setDiscountPrice(product.getPrice());
+		Product saveProduct = productService.saveProduct(product);
+		if (!ObjectUtils.isEmpty(saveProduct)) {
+			File saveFile = new ClassPathResource("static/img").getFile();
+
+			Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "product_img" + File.separator
+					+ imageName);
+
+			// System.out.println(path);
+			Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+			session.setAttribute("succMsg", "Product Saved Success");
+		} else {
+			session.setAttribute("errorMsg", "something wrong on server");
+		}
+		return "redirect:/admin/loadAddProduct";
+	}
+    
+    @GetMapping("/products")
+	public String loadViewProduct(Model m, @RequestParam(defaultValue = "") String ch,
+			@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+		List<Product> products = null;
+		if (ch != null && ch.length() > 0) {
+			products = productService.searchProduct(ch);
+		} else {
+			products = productService.getAllProducts();
+		}
+		m.addAttribute("products", products);
+
+		
+		/*
+		 * Page<Product> page = null; if (ch != null && ch.length() > 0) { page =
+		 * productService.searchProductPagination(pageNo, pageSize, ch); } else { page =
+		 * productService.getAllProductsPagination(pageNo, pageSize); }
+		 * m.addAttribute("products", page.getContent());
+		 * 
+		 * m.addAttribute("pageNo", page.getNumber()); m.addAttribute("pageSize",
+		 * pageSize); m.addAttribute("totalElements", page.getTotalElements());
+		 * m.addAttribute("totalPages", page.getTotalPages()); m.addAttribute("isFirst",
+		 * page.isFirst()); m.addAttribute("isLast", page.isLast());
+		 */
+		 
+
+		return "admin/products";
+	}
+   
+    
 }
